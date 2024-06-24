77da5aa537cb817a9c24fa66f6e5f09747c935ce
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2024-06-24 14:49:14 +0200

    SPRIN-222 - Use axios and fetch with Then keyword

--- src/API/RandomUser.ts
+++ src/API/RandomUser.ts
@@ -10,3 +10,36 @@ export const getRandomUsers = async (): Promise<any> => {
     console.log("Failed to generate random users ", error);
   }
 };
+
+// axios with then
+export const getRandomUsersWithThen = () => {
+  axios
+    .get("https://randomuser.me/api/?results=10")
+    .then((response) => {
+      console.log("Random users: ", response);
+
+      return response;
+    })
+    .catch((error) => {
+      console.log("Failed to generate random users ", error);
+    });
+};
+
+// using fetch
+export const getRandomUsersWithFetch = () => {
+  fetch("https://randomuser.me/api/?results=10")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Server response is not success");
+      }
+      console.log("Random users: ", response);
+
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("Error fetching random users ", error);
+    });
+};
