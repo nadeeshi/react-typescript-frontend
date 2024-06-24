4341d4717a402a614c960b0a2e0c5941fe56f89c
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2024-06-24 10:19:42 +0200

    SPRIN-219 - Pass props using interface with FC component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { FC } from "react";
 
+interface Props {
+  title: string;
+}
 
-const App = () => {
+/**
+ * FC - react use FC to define function component, always implicitly include children in the props
+ * 
+ * @returns 
+ */
+const App: FC<Props> = () => {
   
   return (
     <div>Test App</div>
