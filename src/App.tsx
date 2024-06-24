bdddd12798f1c87006c2cdd7b79038e52871fb22
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2024-06-24 10:23:56 +0200

    SPRIN-219 - remove FC component, sometimes it's unwanted

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+//import { FC } from "react";
 
 interface Props {
   title: string;
@@ -6,10 +6,12 @@ interface Props {
 
 /**
  * FC - react use FC to define function component, always implicitly include children in the props
- * 
+ * sometimes, you don't need to pass children, so that time this can be unwanted rendering
  * @returns 
  */
-const App: FC<Props> = () => {
+
+//const App: FC<Props> = () => {
+const App = (props: Props) => {
   
   return (
     <div>Test App</div>
